86d840be926fcd8f3e077030f0a0efe10eceff02
Cameron Crockrom <cameron.crockrom@hudl.com>
2014-05-23 15:06:23 -0500

    Added wrapper to set env-vars and run.

---
+++ start-bot.bat
@@ -0,0 +1,7 @@
+setx HUBOT_IRC_SERVER "irc.twitch.tv"
+setx HUBOT_IRC_ROOMS "#camtendo,#drewzey"
+setx HUBOT_IRC_NICK "reesaybot"
+setx HUBOT_IRC_UNFLOOD "3000"
+setx HUBOT_IRC_PASSWORD "oauth:9z6rlu7ggz6kfg58rs44ve94lg9fl09"
+
+./bin/hubot -a irc
