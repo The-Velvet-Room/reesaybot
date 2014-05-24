63f668f42a0f418b0120f5d3668f5710dd5d1f9e
Cameron Crockrom <ccrockrom@gmail.com>
2014-05-24 15:57:30 -0500

    Move scripts to working directory.

--- start-bot.bat
+++ start-bot.bat
@@ -3,5 +3,6 @@ setx HUBOT_IRC_ROOMS "#camtendo,#drewzey,#milhouse92,#grandpajakelol"
 setx HUBOT_IRC_NICK "reesaybot"
 setx HUBOT_IRC_UNFLOOD "3000"
 setx HUBOT_IRC_PASSWORD "oauth:9z6rlu7ggz6kfg58rs44ve94lg9fl09"
+xcopy "scripts\*" "node_modules\hubot\src\scripts\" /E /C /H /R /K /O /Y
 
-./bin/hubot -a irc
+./bin/hubot -a irc -r ../
